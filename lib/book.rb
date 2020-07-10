0dc6d154d028d7ee4ac924671f4c187337f66f9c
Doug Schallmoser <dougschallmoser@mgail.com>
2020-07-10 16:52:33 -0700

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,64 @@
+## 1. Define a Book class
+## 2. Create an initialize method with a @title
+## 3. Create methods for:
+##      #title, #author_name, #page_count, #genre
+## 4. Create a method to turn the page.
+
+# REFACTORED CODE BELOW:
+
+class Book
+    attr_accessor :author, :page_count, :genre 
+    attr_reader  :title
+
+    def initialize(title)
+        @title = title
+    end 
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end 
+
+end 
 
 
+## ORIGINAL CODE BELOW:
+
+# class Book
+
+#     def initialize(title)
+#         @title = title
+#     end 
+
+#     def title 
+#         @title
+#     end 
+
+#     def author=(author)
+#         @author = author
+#     end 
+
+#     def author
+#         @author
+#     end 
+
+#     def page_count=(page_num)
+#         @page_count = page_num
+#     end 
+
+#     def page_count
+#         @page_count
+#     end 
+
+#     def genre=(genre)
+#         @genre = genre
+#     end 
+
+#     def genre
+#         @genre
+#     end 
+
+#     def turn_page 
+#         puts "Flipping the page...wow, you read fast!"
+#     end 
+
+# end 
