e4acca3ad477eab9ba7bbe5bc136b66ebb15a2a5
Jackson Reynolds <jwreynold@gmail.com>
2020-07-10 13:11:16 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,10 @@
+class Book
+  attr_accessor :title, :author, :page_count, :genre
+  def initialize(title)
+    @title = title
+  end #initialize
 
-
+  def turn_page
+    puts 'Flipping the page...wow, you read fast!'
+  end #turn_page
+end #Book
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,11 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition
+  def initialize(brand)
+    @brand = brand
+  end #initialize
+
+  def cobble
+    puts 'Your shoe is as good as new!'
+    @condition = 'new'
+  end #cobble
+end #Shoe
